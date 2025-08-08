e7fa10d19f9f84215003dedba08c1681b4942927
aguinaldobrga <guigo.abf@gmail.com>
2025-08-08 14:35:15 -0400

    Adicionados componentes base para estrutura visual do blog

---
+++ src/app/components/small-card/small-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  standalone: true,
+  templateUrl: './small-card.html',
+  styleUrls: ['./small-card.css']
+})
+export class SmallCardComponent {}
